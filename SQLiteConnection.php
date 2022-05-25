e912fd2d10a4e752d892d20f048a41fdae328bb1
Christian Glass <cgl@ekf.de>
2022-05-25 11:06:15 +0200

    ready4test

--- SQLiteConnection.php
+++ SQLiteConnection.php
@@ -1,34 +1,23 @@
 <?php
 
-require_once(__DIR__.'/Database.php');
+// require_once(__DIR__.'/Database.php');
 
 /**
  * SQLite connnection
  */
 class SQLiteConnection extends SQLite3{
-    /**
-     * PDO instance
-     * @var type 
-     */
+    
+    const PATH_TO_SQLITE_FILE = 'database.db';
+
     private $pdo;
     private $db;
-    private bool $delete_db_file = false;
 
     function __construct() {
-        if ($this->delete_db_file) {
-            //echo ("Allowed to delete file"."<br>");
-            if (file_exists(Config::PATH_TO_SQLITE_FILE)){
-                if ($this->delete_db_file && !unlink(Config::PATH_TO_SQLITE_FILE)) {
-                    echo (Config::PATH_TO_SQLITE_FILE." cannot be deleted due to an error"."<br>");
-                }
-                else {
-                    //echo (Config::PATH_TO_SQLITE_FILE." has been deleted"."<br>");
-                }
-                //echo('Reading db file "'.Config::PATH_TO_SQLITE_FILE.'"'."<br>");
-            } else {
-            //echo('Creating file '.Config::PATH_TO_SQLITE_FILE."<br>");
-                fopen(Config::PATH_TO_SQLITE_FILE, "w");
-            }
+        // if (file_exists($PATH_TO_SQLITE_FILE))
+        if (!file_exists(SQLiteConnection::PATH_TO_SQLITE_FILE))
+        {
+            //echo('Reading db file "'.Config::PATH_TO_SQLITE_FILE.'"'."<br>");
+            fopen(SQLiteConnection::PATH_TO_SQLITE_FILE, "w");
         }
      }
 
@@ -42,103 +31,15 @@ class SQLiteConnection extends SQLite3{
         //     $this->db = $this->open(Config::PATH_TO_SQLITE_FILE);
         // }
         $this->db = new SQLiteConnection();
-        $this->open(Config::PATH_TO_SQLITE_FILE);
-        if(!$this->db){
+        $this->open(SQLiteConnection::PATH_TO_SQLITE_FILE);
+        if(!$this->db)
+        {
             echo $this->db->lastErrorMsg();
-        } else {
+        // } else {
             //echo "Opened database successfully"."<br>";
         }
         // return $this->pdo;
     }
-
-    public function insertData() {
-    $sql = 'drop table if exists person;';
-    $this->insert($sql);
-    $sql = 'drop table if exists login;';
-    $this->insert($sql);
-    //////////////////////////////
-    $sql = 'create table if not exists person ('
-    . 'id integer primary key autoincrement,'
-    . 'name text unique,'
-    . 'lastname text'
-    . ');';
-    $this->insert($sql);
-    $sql = 'create table if not exists login ('
-    . 'id integer primary key autoincrement,'
-    . 'p_id integer,'
-    . 'p_name text,'
-    . 'p_lastname text,'
-    . 'p_password text unique,'
-    . 'p_admin default "false",'
-    . 'foreign key (p_id) references person(id)'
-    . ');';
-    $this->insert($sql);
-    //$sql = "insert into person (name, age) values ('bla', 34);";
-    //$this->insert($sql);
-    // $this->insert('.tables');
     }
 
-    public function insert($sql)
-    {
-        echo "<br>"."$sql";
-        $ret = $this->db->exec($sql);
-        if(!$ret){
-            echo $this->db->lastErrorMsg();
-        }
-        $this->db->close();
-    }
-
-    public function getdata() {
-        // $stmt = $this->pdo->query('SELECT project_id, project_name '
-        //         . 'FROM projects');
-        // $stmt = $this->pdo->query('SELECT '
-        // + 'person.id, '
-        // + 'person.name, '
-        // + 'person.lastname, '
-        // + 'login.p_password, '
-        // + 'login.p_admin '
-        // + 'FROM person '
-        // + 'join login on person.id = login.p_id');
-        $persons = [];
-        // while ($row = $stmt->fetchArray(SQLITE3_ASSOC))
-        // {
-            //     $persons[] =
-            //     [
-                //         'name' => $row['name'],
-                //         'age' => $row['age']
-                //     ];
-                // }
-
-        // $dbhandle = new SQLite3(Config::PATH_TO_SQLITE_FILE, 0666, $error);
-        $db = new SQLite3(Config::PATH_TO_SQLITE_FILE);
-        // if (!$dbhandle) die ($error);
-        $sql = 'select '
-        . 'name, '
-        . 'age '
-        . 'from person;';
-        echo("<br>".$sql);
-        $stmt = $this->pdo->query($sql);
-        // $results = $db->query($sql);
-        // echo("</p>");
-        // var_dump($results);
-        // while ($row = $results->fetchArray()) {
-        //     $persons = var_dump($row);
-        // }
-
-        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)){
-            $persons = [
-                'name' => $row['name'],
-                'age' => $row['age']
-            ];
-        }
-        // $result = $tmt->query($sql);
-        // if (!$result) die("Cannot execute query: ".$stmt);
-        // echo('call data row');
-        // while ($row = $result->fetchArray()) {
-        //     echo('call data row');
-        //     var_dump($row);
-        // }
-        // $db->close();
-        return $persons;
-    }
-}
+?>
--- index.php
+++ index.php
@@ -1,99 +1,167 @@
 <html>
  <head>
    <title>Hello World</title>
- </head>
- <body>
-<?php
+   <script
+   src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+   crossorigin="anonymous"></script>
+   <link
+   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+   rel="stylesheet"
+   integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+   crossorigin="anonymous">
+ 	</head>
+ 	<body>
+		<div class="container">
+		<?php
 
-// load class file
-require_once('SQLiteConnection.php');
+			// function connect() {
+			// 	// if (file_exists($PATH_TO_SQLITE_FILE)){
+			// 		echo "<br>".'Load file: '.$PATH_TO_SQLITE_FILE;
+			// 		echo "<br>".'Load file: '.$file;
+			// 	if (file_exists($PATH_TO_SQLITE_FILE)){
+			// 		// fopen($PATH_TO_SQLITE_FILE, "w");
+			// 		fopen($PATH_TO_SQLITE_FILE, "w");
+			// 	} else {
+			// 		echo "<br>".'File does not exist: '.$PATH_TO_SQLITE_FILE;
+			// 	}
+			// }
+			// load class file
+			require_once('SQLiteConnection.php');
+			require_once 'vendor/autoload.php';
 
-// build own structure
-class person{
-private $name;
-private $age;
+			// build own structure
+			class person
+			{
+				private $name;
+				private $surname;
+				private $age;
 
-private function __construct(string $name, int $age){
-	$this->name = $name;
-	$this->age = $age;
-}
-private function get_name(){
-	return $this->name;
-}
-private function get_age(){
-	return $this->age;
-}
-}
+				public function __construct(string $name, string $surname, int $age){
+					$this->name = $name;
+					$this->surname = $surname;
+					$this->age = $age;
+				}
+				public function get_name(){
+					return $this->name;
+				}
+				public function get_surname(){
+					return $this->surname;
+				}
+				public function get_age(){
+					return $this->age;
+				}
+			}
+			
+			echo '<table class="table">';
+			echo '<tr>';
+			echo '<th scope="col">1st name</th>';
+			echo '<th scope="col">last name</th>';
+			echo '<th scope="col">age</th>';
+			echo '<th scope="col">pw</th>';
+			echo '</tr>';
+			
+			// main routine, get db connection
+			getDataIntern();
+			getDataFromDB();
+			getDataRandom();
+			
+			// end html code
+			echo '</table>';
+			echo '</div>';
+			echo '</body>';
 
-// main routine, get db connection
-// $person = new person('achim', 45);
-// $person = new person('paula', 30);
-$pdo = new SQLiteConnection();
-$pdo_connect = $pdo->connect();
-if ($pdo != null){
-	// echo 'Connected to the SQLite database successfully!';
-	// $pdo->insertData();
-	// (new SQLiteConnection())->insertData();
-	// $pdo->getdata();
-	$results = $pdo->query(''
-	. 'select '
-	. 'name, '
-	. 'age '
-	. 'from person;');
-	echo("<br>".$sql);
-	// while ($row = $results->fetchArray()) {
-	// 	var_dump($row);
-	// }
-	echo '<head>';
-	echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>';
-	echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">';
-	echo '</head>';
-	echo '<body>';
-	echo '<div class="container">';
-	echo '<table class="table">';
-	echo '<tr>';
-	echo '<th scope="col">name</th>';
-	echo '<th scope="col">age</th>';
-	echo '</tr>';
-	while($row=$results->fetchArray(SQLITE3_ASSOC)){
-		echo '<tr>';
-		echo "<td>$row[name]</td><td>$row[age]</td>";
-		echo '</tr>';
-	}
-	// echo '<tr>';
-	// echo "<td>".$person1->get_name()."</td><td>".$person1->get_age()."</td>";
-	// echo '</tr>';
-	for($count=0; $count<10; $count++){
-		echo '<tr>';
-		echo "<td>".makeRandomString()."</td><td>".rand(10,100)."</td>";
-		echo '</tr>';
-	}
-	echo '</table>';
-	echo '</div>';
-	echo '</body>';
-}
-else
-{
-	echo('Whoops, could not connect to the SQLite database!');
-	// (new SQLiteConnection())->insertData();
-}
-
-// get random string
-function makeRandomString($max=6)
-{
-	$i = 0;
-	$possible_keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	$keys_length = strlen($possible_keys);
-	$str = "";
-	while($i<$max) {
-		$rand = mt_rand(1,$keys_length-1);
-		$str.= $possible_keys[$rand];
-		$i++;
-	}
-	$str = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 10);
-	return $str;
-}
-
-?>
- </body>
+			// get random string
+			function makeRandomString($max=6)
+			{
+				// $i = 0;
+				// $possible_keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				// $keys_length = strlen($possible_keys);
+				// $str = "";
+				// while($i<$max) {
+				// 	$rand = mt_rand(1,$keys_length-1);
+				// 	$str.= $possible_keys[$rand];
+				// 	$i++;
+				// }
+				$str = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 10);
+				return $str;
+			}
+			// get data
+			function getDataIntern()
+			{
+				$person1 = new person('achim', 'bounty', 45);
+				$person2 = new person('paula', 'ursula', 30);
+				// get data from own class
+				echo '<tr>';
+				echo "<td>".$person1->get_name()."</td>";
+				echo "<td>".$person1->get_surname()."</td>";
+				echo "<td>".$person1->get_age()."</td>";
+				echo '</tr>';
+				echo '<tr>';
+				echo "<td>".$person2->get_name()."</td>";
+				echo "<td>".$person2->get_surname()."</td>";
+				echo "<td>".$person2->get_age()."</td>";
+				echo '</tr>';
+				echo '<tr>';
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo '</tr>';
+			}
+			function getDataFromDB()
+			{
+				// db connection. -> sqlite
+				$pdo = new SQLiteConnection();
+				$pdo_connect = $pdo->connect();
+				if ($pdo != null){
+					// echo 'Connected to the SQLite database successfully!';
+					// $pdo->insertData();
+					// (new SQLiteConnection())->insertData();
+					// $pdo->getdata();
+					$sql = ''
+					. 'select '
+					. 'firstname, '
+					. 'lastname, '
+					. 'age '
+					. 'from person';
+					$results = $pdo->query($sql);
+					echo("<br>".$sql);
+					while($row = $results->fetchArray(SQLITE3_ASSOC)){
+						echo '<tr>';
+						echo "<td>$row[firstname]</td>";
+						echo "<td>$row[lastname]</td>";
+						echo "<td>$row[age]</td>";
+						echo "<td>".makeRandomString()."</td>";
+						echo '</tr>';
+					}
+				}
+				else
+				{
+					echo("<br>".'Whoops, could not connect to the SQLite database!');
+				}
+				echo '<tr>';
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo "<td>-</td>";
+				echo '</tr>';
+			}
+			function getDataRandom()
+			{
+				// get random data with library
+				for($count=0; $count<10; $count++)
+				{
+					$faker = Faker\Factory::create();
+					echo '<tr>';
+					echo "<td>".$faker->firstName."</td>";
+					echo "<td>".$faker->lastName."</td>";
+					echo "<td>".rand()."</td>";
+					echo "<td>".makeRandomString()."</td>";
+					echo '</tr>';
+				}
+			}
+		?>
+	</table>
+</body>
 </html>
